d46d8391098874550e4c3762e22585aaf637634d
gitYoungren <youngrengitonga@gmail.com>
2023-01-26 21:13:00 +0300

    files added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./component/Home";
-import Login from "./component/Login";
-import Navbar from "./component/NavBar";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div>
-      <Navbar setIsLoggedIn={setIsLoggedIn} />
+      <Navbar  />
       <Routes>
         <Route exact path="/logout" element={<logout />} />
-        <Route exact path="/bookshelf" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-        <Route exact path="/" element={<Home isLoggedIn={isLoggedIn} />} />
+        <Route exact path="/bookshelf" element={<Login />} />
+        <Route exact path="/" element={<Home  />} />
       </Routes>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Login() {
+function Home() {
   return (<div>{/*{code here}*/}
-  <h1>Login Page</h1>
+  <h1>Home Page</h1>
   </div>);
 }
 
-export default Login;
+export default Home;
